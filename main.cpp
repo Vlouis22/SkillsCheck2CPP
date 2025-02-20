44e3f3e32fcedc8ed1a5de2c9370985fad0d84cd
Valery Louis <louisvalery2022@gmail.com>
2025-02-20 17:52:08 -0500

    completed task 6

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int task3();
 int task5();
+int task6();
 
 int task3(){
     double num1;
@@ -29,11 +30,33 @@ int task5(){
     return 0;
 }
 
+int task6(){
+    int num1;
+    int num2;
+    cout << "Provide a number: ";
+    cin >> num1;
+    cout << "Provide a number that is bigger than " << num1 << ": ";
+    cin >> num2;
+
+    if(num2 < num1){
+        cout << "the second number must be larger than the first number!";
+        return -1;
+    }
+
+    for(int i = num1; i <= num2; i++){
+        if(i % 3 == 0){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
+
 
 
 
 int main(){
     // task3();
-    task5();
+    // task5();
+    task6();
     return 0;
 }
